70ac04e9fca5716a3acaa74d8f24a36f1f3f2a9a
smudgegame <atmargraff@gmail.com>
2021-04-09 21:59:50 -0400

    Add component skeletons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContentsComponent } from './contents/contents.component';
+import { PersonalComponent } from './personal/personal.component';
+import { EducationComponent } from './education/education.component';
+import { CodingComponent } from './coding/coding.component';
+import { ArtworkComponent } from './artwork/artwork.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContentsComponent,
+    PersonalComponent,
+    EducationComponent,
+    CodingComponent,
+    ArtworkComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/personal/personal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'personal',
+  templateUrl: './personal.component.html',
+  styleUrls: ['./personal.component.css']
+})
+export class PersonalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
