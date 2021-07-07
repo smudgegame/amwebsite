b88e13924bc71604dd71223d9d09d9f08dd91ca0
smudgegame <atmargraff@gmail.com>
2021-07-07 16:21:02 -0400

    Fix ngFor template

--- src/app/artwork/artwork.component.ts
+++ src/app/artwork/artwork.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, PipeTransform, Pipe } from '@angular/core';
-import { Artwork, constants } from './artwork';
+import { Component, OnInit } from '@angular/core';
+import { constants } from './artwork';
 
 @Component({
   selector: 'artwork',
@@ -8,25 +8,9 @@ import { Artwork, constants } from './artwork';
 })
 export class ArtworkComponent implements OnInit {
 
-  // this.artworks = artworks;
   artworks = constants.artworks
-  constructor() {
 
-    // this.artworks = [
-    //   {
-    //     title:"Desert Scene",
-    //     medium:"Asperite",
-    //     description:"Desert scene inspired by Spaceman Spiff",
-    //     image:"assets/artwork/gifs/Desert.gif"
-    //   },
-    //   {
-    //     title:"Rocket Engine",
-    //     medium:"Asperite",
-    //     description: "A fun rocket engine",
-    //     image:"assets/artwork/gifs/Small Rocket Engine.gif"
-    //   }
-    // ];
-  }
+  constructor() {}
 
   ngOnInit(): void { }
 }
--- src/app/artwork/artwork.ts
+++ src/app/artwork/artwork.ts
@@ -15,20 +15,86 @@ export class constants {
         {
             title: "Rocket Engine",
             medium: "Asperite",
-            description: "Rocket engine inspired by SpaceX merlin engine start up",
+            description: "Rocket engine startup inspired by SpaceX merlin engine startup. A puff of unburnt fuel can be seen just before the engine ignites with great force.",
             image: "assets/artwork/gifs/Small Rocket Engine.gif"
         },
         {
             title: "Pig Walk",
             medium: "Asperite",
-            description: "Practice on animating a low resolution quadraped",
+            description: "Animating a quadraped is difficult. This is a first attempt to have the legs alternate. This is more of a pig waddle than a pig trot.",
             image: "assets/artwork/gifs/Pig_Run.gif"
         },
         {
             title: "Walking Speed",
             medium: "Asperite",
-            description: "Matching pixel speed to game world speed is difficult. This animation is used to insure the character moves at the same pixel rate in animation as in the game world",
+            description: "Matching pixel speed to game world speed is difficult. This animation is used to insure the character moves at the same pixel rate in animation as in the game world.",
             image: "assets/artwork/gifs/CharWalk.gif"
+        },
+        {
+            title: "Miniship",
+            medium: "Asperite",
+            description: "In an effort to ''keep animating simple'' I created a very elementary animation of a small ship taking off and opening. In this animation I learned how to use the tag feature in Asperite.",
+            image: "assets/artwork/gifs/Miniship.gif"
+        },
+        {
+            title: "Explosion",
+            medium: "Asperite",
+            description: "Explosion animations don't have to be complicated. Explosions rapidly expand in bright colors and they slowly decay to darker colors and puffs of smoke. Note the slow of the rate of expansion.",
+            image: "assets/artwork/gifs/Explosion.gif"
+        },
+        {
+            title: "Keyhole",
+            medium: "Pencil",
+            description: "A young girl up later than she should be peers through a lit keyhole while holding a well loved bear. This is my sketch for a prompt in an ideas sketchbook.",
+            image: "assets/artwork/images/Keyhole.jpg"
+        },
+        {
+            title: "Judy",
+            medium: "Pencil",
+            description: "Judy Robinson, played by Taylor Russell, in the recent Lost In Space reboot on Netflix.",
+            image: "assets/artwork/images/Judy.JPG"
+        },
+        {
+            title: "Pascal",
+            medium: "Pencil",
+            description: "Blaise Pascal is one of my favorite mathematicians due to his numerous contributions to mathematics, statistics, science, and philosophy.",
+            image: "assets/artwork/images/Pascal.JPG"
+        },
+        {
+            title: "Statue of Liberty",
+            medium: "Ballpoint Pen",
+            description: "Discouraged by the struggles at the boarders of our country, I wanted to sketch the Statue of Liberty. Our country has always been a symbol of freedom. I count myself as extremely fortunate to have been born in a country where freedom is celebrated.",
+            image: "assets/artwork/images/StatueOfLiberty.JPG"
+        },
+        {
+            title: "Korin",
+            medium: "Procreate App",
+            description: "Master Korin atop his tower. This is a scene from the anime tv show Dragonball where Goku climbs Korin's tower to get a drink of the Sacred Water.",
+            image: "assets/artwork/images/Korin.jpg"
+        },
+        {
+            title: "Yajirobe",
+            medium: "Procreate App",
+            description: "''Hey! You ate my breaktist!'' Here's another scene from the tv show Dragonball where the character Yajirobe is first introduced.",
+            image: "assets/artwork/images/Yajirobe.jpg"
+        },
+        {
+            title: "Mines of Moria",
+            medium: "Procreate App",
+            description: "Originally titled ''Here There Be Monsters'' this turned into an interpretation of the Mines of Moria from the book The Lord of the Rings.",
+            image: "assets/artwork/images/Moria.jpg"
+        },
+        {
+            title: "Shackleton",
+            medium: "Procreate App",
+            description: "Sir Ernest Shackleton and a crew of five in the voyage of the James Caird. The James Caird was a small boat about 23 feet long that traveled 800 miles under the command of Sir Ernest Shackleton to seek help rescuing his crew. I was inspired to make this sketch after finishing the book Endurance.",
+            image: "assets/artwork/images/Shackleton.jpg"
+        },
+        {
+            title: "Traveler",
+            medium: "Procreate App",
+            description: "This is one of my first sketches in the Procreate App. I wanted to find a fun cell-shaded style to sketch ideas that come into my head.",
+            image: "assets/artwork/images/Traveler.jpg"
         }
     ];
 }
