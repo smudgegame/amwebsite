b87bd516761ea949c5f0cb3619e0c2b0b5cb78e2
smudgegame <atmargraff@gmail.com>
2026-02-04 21:02:03 -0500

    Clean up react app and add sections

---
+++ react-website/amwebsite/src/Sections/Professional/ProfessionalSection.tsx
@@ -0,0 +1,16 @@
+import './professional.css'
+
+export default function ProfessionalSection() {
+  return (
+    <section className="professional">
+      <h1>Aaron Margraff</h1>
+      <p>Software Engineer</p>
+
+      <pre className="terminal">
+        {`> building systems
+        > learning relentlessly
+        > shipping carefully`}
+      </pre>
+    </section>
+  )
+}
